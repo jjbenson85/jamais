2825f3ccb51de99a8253dd7070342c74f73c7993
jjbenson85 <jjbenson85@gmail.com>
2024-03-01 17:18:44 +0000

    Add cls utility function for handling CSS classes

---
+++ src/cls.ts
@@ -0,0 +1,15 @@
+const handleClsObj = (classes: Record<string, boolean>): string[] =>
+  Object.entries(classes)
+    .filter(([_key, value]) => value)
+    .map(([key]) => key);
+
+const handleClsArr = (classes: Classes[]): string[] =>
+  classes.flatMap((cls) => {
+    if (typeof cls === "string") return cls;
+    if (Array.isArray(cls)) return handleClsArr(cls);
+    return handleClsObj(cls);
+  });
+
+type Classes = Record<string, boolean> | string | Classes[];
+export const cls = (classes: Classes): string =>
+  [...new Set(handleClsArr([classes]))].join(" ");
