7e6d1f21837a8534a202c302b605708612aea0d0
jjbenson85 <jjbenson85@gmail.com>
2024-03-02 13:48:59 +0000

    Add bindClasses import and call in setupRefs

--- src/setupRefs.ts
+++ src/setupRefs.ts
@@ -1,3 +1,4 @@
+import { bindClasses } from "./bindClasses";
 import { bindText } from "./bindText";
 import { globalQueue } from "./processQueue";
 import { Ref, isRef } from "./ref";
@@ -19,17 +20,6 @@ const bindModels = (value: Ref<any>) => {
   };
 };
 
-const bindClasses = (value: Ref<unknown>) => (el: Element) => {
-  const fn = () => {
-    const curr = String(value.value).trim();
-    const prev = String(value.previousValue).trim();
-    prev && prev.split(" ").forEach((cls) => el.classList.remove(cls));
-    curr && curr.split(" ").forEach((cls) => el.classList.add(cls));
-  };
-  value.addWatcher(() => globalQueue.add(fn));
-  fn();
-};
-
 const isRefEntry = (
   entry: [string, SetupBits]
 ): entry is [string, Ref<unknown>] => isRef(entry[1]);
@@ -38,10 +28,11 @@ export function setupRefs(dataEntries: [string, SetupBits][], el: Element) {
   const refs = dataEntries.filter(isRefEntry);
 
   bindText(el, Object.fromEntries(dataEntries));
+  bindClasses(el, Object.fromEntries(dataEntries));
 
   for (const [key, value] of refs) {
     el.querySelectorAll(`[data-model="${key}"]`).forEach(bindModels(value));
 
-    el.querySelectorAll(`[data-class=${key}]`).forEach(bindClasses(value));
+    // el.querySelectorAll(`[data-class=${key}]`).forEach(bindClasses(value));
   }
 }
