91e3c784053d1a2a3ae97165f6d3779554752cd1
jjbenson85 <jjbenson85@gmail.com>
2024-03-02 13:45:57 +0000

    Add getElementsToBind function and its test

---
+++ src/getElementsToBind.ts
@@ -0,0 +1,26 @@
+import { getPropertyFromPath } from "./helpers";
+import { SetupBits } from "./setup";
+
+export type BindType = "text" | "class";
+export const getElementsToBind = (
+  el: Element,
+  bindType: BindType,
+  data: Record<string, SetupBits>,
+  insideFor: boolean
+) => {
+  const elz = [el, ...el.querySelectorAll(`[data-${bindType}]`)];
+  return elz.reduce((acc, element) => {
+    const _attrValue = element.getAttribute(`data-${bindType}`);
+    if (!_attrValue) return acc;
+    if (!insideFor && _attrValue.startsWith("$")) return acc;
+
+    const attrValue = _attrValue.replace("$", "__data-for__");
+
+    const value = data[attrValue.split(".")[0]];
+    const getDeepValue = () => getPropertyFromPath(data, attrValue);
+
+    const obj = { el: element, value, getDeepValue };
+    acc.push(obj);
+    return acc;
+  }, [] as { el: Element; value: SetupBits; getDeepValue: () => unknown }[]);
+};
