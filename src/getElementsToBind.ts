442ce130781676c36546df12cc3b6b1287eb5256
jjbenson85 <jjbenson85@gmail.com>
2024-03-03 02:02:45 +0000

    Add bindIf function to handle conditional rendering

--- src/getElementsToBind.ts
+++ src/getElementsToBind.ts
@@ -1,7 +1,14 @@
 import { getPropertyFromPath, isObject, toPrevValue, toValue } from "./helpers";
 import { SetupBits } from "./setup";
 
-export type BindType = "text" | "class" | "model" | "in";
+export type BindType =
+  | "text"
+  | "class"
+  | "model"
+  | "in"
+  | "if"
+  | "else-if"
+  | "else";
 
 type ElementToBindItem = {
   el: Element;
@@ -9,21 +16,42 @@ type ElementToBindItem = {
   getDeepValue: () => unknown;
   getDeepPreviousValue: () => unknown;
 };
-export const getElementsToBind = (
-  el: Element,
-  bindType: BindType,
-  data: Record<string, SetupBits>,
-  insideFor: boolean
-) => {
-  const elz = [el, ...el.querySelectorAll(`[data-${bindType}]`)];
-  return elz.reduce((acc, element) => {
-    const _attrValue = element.getAttribute(`data-${bindType}`);
-    if (!_attrValue) return acc;
-    if (!insideFor && _attrValue.startsWith("$")) return acc;
 
-    const attrValue = _attrValue.replace("$", "__data-for__");
+export const getSiblingElsWithBindType = (
+  el: Element,
+  bindType: BindType
+): Element[] => {
+  const els = [];
+  let nextEl = el.nextElementSibling;
+  while (nextEl) {
+    const attr = nextEl.getAttribute(`data-${bindType}`);
+    if (attr !== null) {
+      els.push(nextEl);
+    }
+    nextEl = nextEl.nextElementSibling;
+  }
+  return els;
+};
+
+const getElementsWithBindType = (
+  el: Element,
+  bindType: BindType
+): Element[] => {
+  const els = el.querySelectorAll(`[data-${bindType}]`);
+  return [el, ...els];
+};
+
+const attachWatchersToEls = (
+  data: Record<string, SetupBits>,
+  els: Element[],
+  bindType: BindType
+) => {
+  return els.reduce((acc, element) => {
+    const attrValue = element.getAttribute(`data-${bindType}`);
+    if (!attrValue) return acc;
+
     const [key, restKey] = attrValue.split(".", 2);
-    
+
     if (!(key in data)) return acc;
 
     const value = data[key];
@@ -47,3 +75,22 @@ export const getElementsToBind = (
     return acc;
   }, [] as ElementToBindItem[]);
 };
+
+export const getElementsToBind = (
+  el: Element,
+  bindType: BindType,
+  data: Record<string, SetupBits>
+) => {
+  const elz = getElementsWithBindType(el, bindType);
+  return attachWatchersToEls(data, elz, bindType);
+};
+
+export const getSiblingElementsToBind = (
+  el: Element,
+  bindType: BindType,
+  data: Record<string, SetupBits>
+) => {
+  const elz = getSiblingElsWithBindType(el, bindType);
+  return attachWatchersToEls(data, elz, bindType);
+};
+
