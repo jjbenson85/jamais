85dd925f73ed47e82cbf9380595f0f1b36bc5a08
jjbenson85 <jjbenson85@gmail.com>
2024-03-05 21:07:44 +0000

    Add eventDirective test

---
+++ src/test/directives/eventDirective.test.ts
@@ -0,0 +1,34 @@
+import "../extendMatchers";
+
+import { DirectiveContext } from "../../bindDirectives";
+import { eventDirective } from "../../directives/eventDirective";
+import { describe, it, expect, vi } from "vitest";
+import { JSDOM } from "jsdom";
+
+describe("eventDirective", () => {
+  it("should bind a click event to an element", () => {
+    const el = new JSDOM().window.document.createElement("button");
+    const data = { handleClick: vi.fn() };
+
+    const ctx: DirectiveContext = {
+      data,
+      el,
+      attrs: [
+        {
+          value: "handleClick",
+          attrPrefix: "click",
+          attrValue: "handleClick",
+          attrModifiers: [],
+          get: () => data.handleClick,
+          getPrevious: () => undefined,
+          effect: () => {},
+        },
+      ],
+      directives: {},
+    };
+
+    eventDirective(ctx);
+    el.click();
+    expect(data.handleClick).toHaveBeenCalled();
+  });
+});
