4f28b48cdb1ce9933344a08ee35858519a2bba76
jjbenson85 <jjbenson85@gmail.com>
2024-03-06 21:35:26 +0000

    Refactor switchDirective to use displayElement helper function

--- src/directives/switchDirective.ts
+++ src/directives/switchDirective.ts
@@ -1,20 +1,22 @@
 import { createDirective } from "../bindDirectives";
+import { displayElement } from "../helpers/displayElement";
 
 export const switchDirective = createDirective((ctx) => {
   const { el, attrs } = ctx;
   for (const attr of attrs) {
     const { get, effect } = attr;
-    const originalDisplay = el.style.display;
     const caseEls = el.querySelectorAll<HTMLElement>("[data-case]");
 
-    const cb = () => {
-      caseEls.forEach((caseEl) => {
-        const caseValue = caseEl.getAttribute("data-case");
-        if (!caseValue) return;
-        const value = get();
-        caseEl.style.display = value === caseValue ? originalDisplay : "none";
-      });
-    };
+    const cbs = [...caseEls].reduce((acc, caseEl) => {
+      const caseValue = caseEl.getAttribute("data-case");
+      if (!caseValue) return acc;
+
+      const displayCaseEl = displayElement(caseEl);
+      acc.push(() => displayCaseEl(get() === caseValue));
+
+      return acc;
+    }, [] as Array<() => string | void>);
+    const cb = () => cbs.forEach((cb) => cb());
     effect(cb);
     cb();
   }
--- src/helpers/displayElement.ts
+++ src/helpers/displayElement.ts
@@ -1,4 +1,4 @@
-export const displayElement = (el: HTMLElement | undefined) => {
+export const displayElement = (el: HTMLElement) => {
   if (!el) return () => {};
   const display = el.style.display;
   return (isVisible: boolean) =>
