e88d7357c46804483d15b39fcd1be699643eab72
jjbenson85 <jjbenson85@gmail.com>
2024-03-11 21:45:00 +0000

    Refactor bindAttributes tests

--- src/test/bindAttributes.test.ts
+++ src/test/bindAttributes.test.ts
@@ -2,42 +2,53 @@ import "./extendMatchers";
 
 import { JSDOM } from "jsdom";
 import { describe, expect, it } from "vitest";
-import { DirectiveContext } from "../bindDirectives";
 import { bindAttributes } from "../bindAttributes";
 
-describe.todo("bindDirective", () => {
-  // it("should bind a value to an element", () => {
-  //   const el = new JSDOM().window.document.createElement("div");
-  //   const data = { foo: "bar" };
-  //   const ctx: DirectiveContext = {
-  //     data,
-  //     el,
-  //     dataValue: undefined,
-  //     attrValue: "aria-label:foo",
-  //     get: () => data.foo,
-  //     getPrevious: () => data.foo,
-  //     effect: () => {},
-  //     directives: {},
-  //     components: {},
-  //   };
-  //   bindDirective(ctx);
-  //   expect(el.outerHTML).toBeHTML(`<div aria-label="bar"></div>`);
-  // });
-  // it("should bind multiple values to an element", () => {
-  //   const el = new JSDOM().window.document.createElement("div");
-  //   const data = { foo: "bar", baz: "qux" };
-  //   const ctx: DirectiveContext = {
-  //     data,
-  //     el,
-  //     dataValue: undefined,
-  //     attrValue: "aria-label:foo label:baz",
-  //     get: () => undefined,
-  //     getPrevious: () => undefined,
-  //     effect: () => {},
-  //     directives: {},
-  //     components: {},
-  //   };
-  //   bindDirective(ctx);
-  //   expect(el.outerHTML).toBeHTML(`<div aria-label="bar" label="qux"></div>`);
-  // });
+describe("bindAttributes", () => {
+  it("should bind a value to an element", () => {
+    const doc = new JSDOM(`<div :aria-label="foo"></div>`).window.document;
+    const el = doc.querySelector<HTMLElement>("div");
+    if (!el) throw new Error("No element found");
+    const data = { foo: "bar" };
+    const ctx = {
+      data,
+      el: doc.body,
+      directives: {},
+      components: {},
+    };
+    bindAttributes(ctx);
+    expect(el.outerHTML).toBeHTML(
+      `<div :aria-label="foo" aria-label="bar"></div>`,
+    );
+  });
+
+  it("should bind multiple values to an element", () => {
+    const doc = new JSDOM(`<div :aria-label="foo" :aria-hidden="baz"></div>`)
+      .window.document;
+    const el = doc.querySelector<HTMLElement>("div");
+    if (!el) throw new Error("No element found");
+
+    bindAttributes({
+      data: { foo: "bar", baz: "true" },
+      el: doc.body,
+      directives: {},
+    });
+    expect(el.outerHTML).toBeHTML(
+      `<div :aria-label="foo" :aria-hidden="baz" aria-label="bar" aria-hidden="true"></div>`,
+    );
+  });
+
+  it("should not bind a value to an element if it's a directive name", () => {
+    const doc = new JSDOM(`<div :data-text="foo"></div>`).window.document;
+    const el = doc.querySelector<HTMLElement>("div");
+    if (!el) throw new Error("No element found");
+
+    bindAttributes({
+      data: { foo: "bar" },
+      el: doc.body,
+      directives: { "data-text": () => {} },
+    });
+
+    expect(el.outerHTML).toBeHTML(`<div :data-text="foo"></div>`);
+  });
 });
