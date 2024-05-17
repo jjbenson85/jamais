283ec917b103361eb4cb89a2d98dc6329bf5a380
jjbenson85 <jjbenson85@gmail.com>
2024-05-17 12:29:23 +0100

    Update ifDirective tests

--- src/test/directives/ifDirective.test.ts
+++ src/test/directives/ifDirective.test.ts
@@ -16,7 +16,7 @@ describe("ifDirective", () => {
     expect(ifDirective.matcher(attr)).toBe(true);
   });
 
-  it("should apply the initial data-if", () => {
+  it.only("should apply the initial data-if", () => {
     globalThis.document.body.innerHTML =
       '<div :data-if="show.get()">Test</div>';
     const el = document.querySelector<HTMLElement>("div");
@@ -25,10 +25,11 @@ describe("ifDirective", () => {
     if (!el || !attr) throw new Error("No element found");
 
     const data = { show: signal(true) };
-    const cb = ifDirective.mounted(el, attr.name, attr.value, data);
+    const cb = ifDirective.mounted(el, attr.name, attr.value, data, {});
     cb && createEffect(cb);
 
-    expect(el.style.display).toBe("unset");
+    expect(document.querySelector("[\\:data-if]"))
+      .toBeTruthy();
   });
 
   it("should remove the element when the value is false", async () => {
@@ -42,12 +43,12 @@ describe("ifDirective", () => {
     const show = signal(true);
 
     const data = { show };
-    const cb = ifDirective.mounted(el, attr.name, attr.value, data);
+    const cb = ifDirective.mounted(el, attr.name, attr.value, data,{});
     cb && createEffect(cb, "ifDirective");
 
     show.set(false);
 
-    expect(el.style.display).toBe("none");
+    expect(document.querySelector('[\\:data-if]')).toBe(null);
   });
 
   it("should hide the else element when value is initialised as true", async () => {
@@ -64,13 +65,11 @@ describe("ifDirective", () => {
     const show = signal(true);
 
     const data = { show };
-    const cb = ifDirective.mounted(el, attr.name, attr.value, data);
+    const cb = ifDirective.mounted(el, attr.name, attr.value, data,{});
     cb && createEffect(cb, "ifDirective");
 
-    expect((parent.firstElementChild as HTMLElement).style.display).toBe(
-      "unset",
-    );
-    expect((parent.lastElementChild as HTMLElement).style.display).toBe("none");
+    expect(document.querySelector('[\\:data-else]')).toBeFalsy();
+    expect(document.querySelector('[\\:data-if]')).toBeTruthy();
   });
 
   it("should show the else element when value is initialised as false", async () => {
@@ -87,15 +86,11 @@ describe("ifDirective", () => {
     const show = signal(false);
 
     const data = { show };
-    const cb = ifDirective.mounted(el, attr.name, attr.value, data);
+    const cb = ifDirective.mounted(el, attr.name, attr.value, data,{});
     cb && createEffect(cb, "ifDirective");
 
-    expect((parent.firstElementChild as HTMLElement).style.display).toBe(
-      "none",
-    );
-    expect((parent.lastElementChild as HTMLElement).style.display).toBe(
-      "unset",
-    );
+    expect(document.querySelector("[\\:data-if]")).toBeFalsy();
+    expect(document.querySelector("[\\:data-else]")).toBeTruthy();
   });
 
   it("should show the else element when value is updated to false", async () => {
@@ -112,17 +107,13 @@ describe("ifDirective", () => {
     const show = signal(true);
 
     const data = { show };
-    const cb = ifDirective.mounted(el, attr.name, attr.value, data);
+    const cb = ifDirective.mounted(el, attr.name, attr.value, data,{});
     cb && createEffect(cb, "ifDirective");
 
     show.set(false);
 
-    expect((parent.firstElementChild as HTMLElement).style.display).toBe(
-      "none",
-    );
-    expect((parent.lastElementChild as HTMLElement).style.display).toBe(
-      "unset",
-    );
+    expect(document.querySelector("[\\:data-if]")).toBeFalsy();
+    expect(document.querySelector("[\\:data-else]")).toBeTruthy();
   });
 
   it("should hide the else element when value is updated to true", async () => {
@@ -139,15 +130,15 @@ describe("ifDirective", () => {
     const show = signal(false);
 
     const data = { show };
-    const cb = ifDirective.mounted(el, attr.name, attr.value, data);
+    const cb = ifDirective.mounted(el, attr.name, attr.value, data,{});
     cb && createEffect(cb, "ifDirective");
 
     show.set(true);
 
-    expect((parent.firstElementChild as HTMLElement).style.display).toBe(
-      "unset",
-    );
-    expect((parent.lastElementChild as HTMLElement).style.display).toBe("none");
+    expect(document.querySelector("[\\:data-if]"))
+      .toBeTruthy();
+    expect(document.querySelector("[\\:data-else]"))
+      .toBeFalsy();
   });
 
   it("should show else-if when value is updated to true", async () => {
@@ -166,16 +157,18 @@ describe("ifDirective", () => {
     const showElseIf = signal(true);
 
     const data = { show, showElseIf };
-    const cb = ifDirective.mounted(el, attr.name, attr.value, data);
+    const cb = ifDirective.mounted(el, attr.name, attr.value, data,{});
     cb && createEffect(cb, "ifDirective");
 
     show.set(false);
 
-    expect((parent.firstElementChild as HTMLElement).style.display).toBe(
-      "none",
-    );
-    expect((parent.children[1] as HTMLElement).style.display).toBe("unset");
-    expect((parent.lastElementChild as HTMLElement).style.display).toBe("none");
+    expect(document.querySelector("[\\:data-if]"))
+      .toBeFalsy();
+    expect(document.querySelector("[\\:data-else-if]"))
+      .toBeTruthy();
+    expect(document.querySelector("[\\:data-else]"))
+      .toBeFalsy();
+
   });
 
   it("should hide else-if when value is updated to false", async () => {
@@ -194,18 +187,17 @@ describe("ifDirective", () => {
     const showElseIf = signal(true);
 
     const data = { show, showElseIf };
-    const cb = ifDirective.mounted(el, attr.name, attr.value, data);
+    const cb = ifDirective.mounted(el, attr.name, attr.value, data,{});
     cb && createEffect(cb, "ifDirective");
 
     showElseIf.set(false);
 
-    expect((parent.firstElementChild as HTMLElement).style.display).toBe(
-      "none",
-    );
-    expect((parent.children[1] as HTMLElement).style.display).toBe("none");
-    expect((parent.lastElementChild as HTMLElement).style.display).toBe(
-      "unset",
-    );
+    expect(document.querySelector("[\\:data-if]"))
+      .toBeFalsy();
+    expect(document.querySelector("[\\:data-else-if]"))
+      .toBeFalsy();
+    expect(document.querySelector("[\\:data-else]"))
+      .toBeTruthy();
   });
 
   it("should handle multiple else-if", async () => {
@@ -226,16 +218,19 @@ describe("ifDirective", () => {
     const showElseIf2 = signal(true);
 
     const data = { show, showElseIf, showElseIf2 };
-    const cb = ifDirective.mounted(el, attr.name, attr.value, data);
+    const cb = ifDirective.mounted(el, attr.name, attr.value, data,{});
     cb && createEffect(cb, "ifDirective");
 
     show.set(false);
 
-    expect((parent.firstElementChild as HTMLElement).style.display).toBe(
-      "none",
-    );
-    expect((parent.children[1] as HTMLElement).style.display).toBe("none");
-    expect((parent.children[2] as HTMLElement).style.display).toBe("unset");
-    expect((parent.lastElementChild as HTMLElement).style.display).toBe("none");
+    expect(document.querySelector("[\\:data-if]"))
+      .toBeFalsy();
+    expect(document.querySelector("[\\:data-else-if=\"showElseIf.get()\"]"))
+      .toBeFalsy();
+    expect(document.querySelector("[\\:data-else-if=\"showElseIf2.get()\"]"))
+      .toBeTruthy();
+    expect(document.querySelector("[\\:data-else]"))
+      .toBeFalsy();
+
   });
 });
